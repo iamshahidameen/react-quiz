566b47827daf0b4da8778de00fc587ce3d9e233e
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-16 20:27:38 +0500

    Added basic structure for Quiz App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from 'react';
+import { useGlobalContext } from './context';
 
-import SetupForm from './SetupForm'
-import Loading from './Loading'
-import Modal from './Modal'
+import SetupForm from './SetupForm';
+import Loading from './Loading';
+import Modal from './Modal';
 function App() {
-  return <h2>quiz starter</h2>
+  return (
+    <main>
+      <section className="quiz quiz-small">
+        <form className="setup-form">
+          <h2>setup quiz</h2>
+          <div className="form-control">
+            <label for="amount">number of questions</label>
+            <input
+              type="number"
+              name="amount"
+              id="amount"
+              className="form-input"
+              min="1"
+              max="50"
+              value="10"
+            />
+          </div>
+          <div className="form-control">
+            <label for="category">category</label>
+            <select name="category" id="category" className="form-input">
+              <option value="sports">sports</option>
+              <option value="history">history</option>
+              <option value="politics">politics</option>
+            </select>
+          </div>
+          <div className="form-control">
+            <label for="difficulty">select difficulty</label>
+            <select name="difficulty" id="difficulty" className="form-input">
+              <option value="easy">easy</option>
+              <option value="medium">medium</option>
+              <option value="hard">hard</option>
+            </select>
+          </div>
+          <button type="submit" className="submit-btn">
+            start
+          </button>
+        </form>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
